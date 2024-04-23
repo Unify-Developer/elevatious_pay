549f03ec90d6f1d23c0796d5d26ff8024e007a07
Unify_developer <fatokiakolade@gmail.com>
2024-04-23 16:33:14 +0100

    Elevatious

--- dashboard.php
+++ dashboard.php
@@ -1,12 +1,4 @@
 <?php
-<<<<<<< HEAD
-require './include/config.php';
-
-// if(!$_SESSION['admin_login']){
-//     header("location: ./index.php");
-//     return;
-// }
-=======
 
 // session_start();
 
@@ -23,7 +15,6 @@ require './include/config.php';
 <?php
 require './include/config.php';
 
->>>>>>> 3bfbe1f9c793ae2222d620357b2e4672782d448e
 ?>
 
 <html lang="en">
@@ -97,7 +88,6 @@ git
 
                 <hr>
 
-<<<<<<< HEAD
                 <ul  class="lois">
                     <!-- class="" style="margin-right: 30px;" -->
                     <li>
@@ -238,146 +228,6 @@ git
                                             </div>
                                             <div class="col">
                                                 <!-- <div class="progress progress-sm mr-2">
-=======
-                <ul>
-                    <!-- class="" style="margin-right: 30px;" -->
-                    <p>
-                    <div class="container-fluid   " style="height: 160px; padding: 10px; background-color: rgb(133, 158, 248); border-radius: 8px; margin-bottom: 10px;">
-                        <h2 class="text-white"> <?php ?> Welcome UNIFY </h2>
-                    </div>
-                    </p>
-
-
-
-                    <h3 class="mt-4 " style="font-family: monospace;"><u>Quick Links</u></h3>
-                    <div class="row">
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-success shadow py-2 quick_links">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-wifi fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="text-center"> Airtime </p>
-                        </div>
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-info shadow py-2 quick_links">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-arrow-alt-circle-down fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="text-center"> Data Subscription </P>
-                        </div>
-
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-warning shadow py-2 quick_links">
-
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="text-center">Electricity bills </P>
-                        </div>
-                        <div class="col-md-3 col-6 mb-4 pt-4" >
-                            <div class="card border-left-primary shadow  py-2 quick_links" >
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <p class="text-center"> WAEC PIN</P>
-                        </div>
-
-                    </div>
-
-                </ul>
-
-
-                <hr>
-
-                <ul>
-                    <h3 class="mt-4" style="font-family: monospace; text-align: center;"><u>Other Services</u></h3>
-                    <div class="row">
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                <!-- Earnings (Monthly) -->
-                                            </div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                <!-- $40,000 -->
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                <!-- Earnings (Annual) -->
-                                            </div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                <!-- $215,000 -->
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Pending Requests Card Example -->
-                        <div class="col-md-3 col-6 mb-4 pt-4">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                <!-- Tasks -->
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                                        <!-- 50% -->
-                                                    </div>
-                                                </div>
-                                                <div class="col">
-                                                    <!-- <div class="progress progress-sm mr-2">
->>>>>>> 3bfbe1f9c793ae2222d620357b2e4672782d448e
                                             <div class="progress-bar bg-info" role="progressbar"
                                                 style="width: 50%" aria-valuenow="50" aria-valuemin="0"
                                                 aria-valuemax="100"></div>
