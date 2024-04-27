31ac8e43f133be7c0ebd563a83d1370577fdc6c6
Unify_developer <fatokiakolade@gmail.com>
2024-04-27 12:35:37 +0100

    Elevatious

--- airtime.php
+++ airtime.php
@@ -36,8 +36,7 @@ require './include/config.php';
 
                     <li class="nav-pushmenu">
                         <div class="push">
-                            <a class="nav-push" data-widget="pushmenu" href="#" role="button"><i
-                                    class="fas fa-bars"></i></a>
+                            <a class="nav-push" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                         </div>
                     </li>
 
@@ -47,12 +46,10 @@ require './include/config.php';
                     </li>
 
                     <li class="nav-item dropdown no-arrow ml-auto ">
-                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <span class="mr-2 d-none d-lg-inline text-gray-600 bold">Vincent
                                 TV</span>
-                            <img class="img-profile rounded-circle" src="./asset/img/undraw_profile.svg"
-                                style="height: 30px; width: 30px;">
+                            <img class="img-profile rounded-circle" src="./asset/img/undraw_profile.svg" style="height: 30px; width: 30px;">
                         </a>
                         <div class="dropdown-menu dropdown-menu shadow animated--grow-in">
                             <a class="dropdown-item" href="#">
@@ -87,57 +84,57 @@ require './include/config.php';
                                 <hr>
                             </div>
                             <div class="class-buttons">
-                            <div class="vincent_airt" style="display: flex;">
-                                <div class="buttonss" >
-                                    <div class="fff">
-                                        <button type="button" class="mtn btn btn-warning ">MTN</button>
+                                <div class="vincent_airt" style="display: flex;">
+                                    <div class="buttonss">
+                                        <div class="fff">
+                                            <button type="button" class="mtn btn btn-warning ">MTN</button>
+                                        </div>
+                                        <div class="fff">
+                                            <button type="button" class="airtel btn btn-danger">AIRTEL</button>
+                                        </div>
+                                        <div class="fff">
+                                            <button type="button" class="glo btn btn-success">GLO</button>
+                                        </div>
+                                        <div class="fff">
+                                            <button type="button" class="9mobile btn btn-secondary">9Mobile</button>
+                                        </div>
                                     </div>
-                                    <div class="fff">
-                                        <button type="button" class="airtel btn btn-danger">AIRTEL</button>
+                                    <div class="">
+                                        <div class="amt">
+                                            <h6>Amount</h6>
+                                            <input class="amount form-control" type="number" name="phone_number">
+                                        </div>
                                     </div>
-                                    <div  class="fff">
-                                       <button type="button" class="glo btn btn-success">GLO</button>
-                                    </div>
-                                    <div class="fff">
-                                        <button type="button" class="9mobile btn btn-secondary" >9Mobile</button>
+                                </div>
+                                <div class="netw_ork mt-3">
+                                    <h6>Network</h6>
+                                    <select class="seee">
+                                        <option value="">MTN</option>
+                                        <option value="">AIRTEL</option>
+                                        <option value="">GLO</option>
+                                        <option value="">9Mobile</option>
+
+                                    </select>
+                                </div>
+                                <div class="">
+                                    <div class="mt-4">
+                                        <h6>Phone Number</h6>
+                                        <input class="phone_number form-control" type="number" name="phone_number">
                                     </div>
                                 </div>
                                 <div class="">
-                                    <div class="amt">
-                                        <h6>Amount</h6>
-                                        <input class="amount form-control" type="number" name="phone_number">
+                                    <div class="mt-4">
+                                        <h6>Amount to pay </h6>
+                                        <!-- For the Amount not him to input it  -->
+                                        <!-- Remeber ooooooooooooooooooooooooooooooooooooooooooooo -->
+                                        <input class="phone_number form-control" type="number" name="phone_number">
                                     </div>
                                 </div>
-                            </div>
-                            <div class="netw_ork mt-3">
-                                <h6>Network</h6>
-                                <select class="seee">
-                                    <option value="">MTN</option>
-                                    <option value="">AIRTEL</option>
-                                    <option value="">GLO</option>
-                                    <option value="">9Mobile</option>
-
-                                </select>
-                            </div>
-                            <div class="">
-                                <div class="mt-4">
-                                    <h6>Phone Number</h6>
-                                    <input class="phone_number form-control" type="number" name="phone_number">
+                                <div class="mt-5">
+                                    <button class="btn btn-primary">
+                                        <label for="">PROCEED</label>
+                                    </button>
                                 </div>
-                            </div>
-                            <div class="">
-                                <div class="mt-4">
-                                    <h6>Amount to pay </h6>
-                                    <!-- For the Amount not him to input it  -->
-                                    <!-- Remeber ooooooooooooooooooooooooooooooooooooooooooooo -->
-                                    <input class="phone_number form-control" type="number" name="phone_number">
-                                </div>
-                            </div>
-                            <div class="mt-5">
-                                <button class="btn btn-primary">
-                                    <label for="">PROCEED</label>
-                                </button>
-                            </div>
                             </div>
                         </div>
 
@@ -145,9 +142,9 @@ require './include/config.php';
                 </ul>
 
 
-            <footer class="Footer text-center">
-                &copy; Elevatious - Pay | Tech Net &reg; 2024
-            </footer>
+                <footer class="Footer text-center">
+                    &copy; Elevatious - Pay | Tech Net &reg; 2024
+                </footer>
             </div>
 
         </nav>
@@ -172,8 +169,7 @@ require './include/config.php';
 
                 <div class="form-inline">
                     <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                            aria-label="Search">
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                         <div class="input-group-append">
                             <button class="btn btn-sidebar">
                                 <i class="fas fa-search fa-fw"></i>
@@ -183,8 +179,7 @@ require './include/config.php';
                 </div>
 
                 <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                        data-accordion="false">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
                         <li class="nav-side">
                             <a href="dashboard.php" class="nav-link">
